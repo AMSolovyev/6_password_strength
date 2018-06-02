fda884b0356aa1bd3d15ec0e4b2c002d13b2d4bb
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-02 22:53:10 +0300

    I renamed have- to has, deleted useless function

--- password_strength.py
+++ password_strength.py
@@ -1,21 +1,21 @@
 import re
 import os
+import getpass
 
 
-def load_datum(file_path):
+def load_data(file_path):
     with open(file_path, 'r') as file:
         return file.read().split('\n')
 
 
-def input_from_file(text):
+def get_added_data_from_file(text):
     work_file = input(text)
-
     if not os.path.exists(work_file):
         print('Файл не существует {}'.format(work_file))
         input_user_datum = input('Введите данные через пробел').split()
         print('=' * 100)
     else:
-        input_user_datum = load_datum(work_file)
+        input_user_datum = load_data(work_file)
 
     return input_user_datum
 
@@ -24,63 +24,57 @@ def is_password_long(password):
     return len(password) >= 6
 
 
-def have_password_symbols(password):
-    if password.isalnum == 0:
-        print('Вы ввели недопустимый пароль!!! Введите его заново')
-    return password
-
-
-def have_both_upper_and_lower_cases(password):
+def has_both_upper_and_lower_cases(password):
     return not password.islower() and not password.isupper()
 
 
-def have_numerical_digits(password):
+def has_numbers(password):
     return password.isdigit()
 
 
-def have_special_characters(password):
+def has_special_characters(password):
     return bool(re.search(r'[ !#$%&():*+,-./[\\\]^_`{|}~]', password))
 
 
-def have_forbidden_words(password, forbidden_words):
+def has_forbidden_words(password, forbidden_words):
     pattern_dd_mm_yyyy = \
         '(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d'
     pattern_mm_dd_yyyy = \
         '(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d'
-    have_forbidden_words = bool(re.search(pattern_dd_mm_yyyy, password))
-    have_forbidden_words = bool(re.search(pattern_mm_dd_yyyy, password))
+    has_forbidden_words = bool(re.search(pattern_dd_mm_yyyy, password))
+    has_forbidden_words = bool(re.search(pattern_mm_dd_yyyy, password))
 
     if password in forbidden_words:
-        have_forbidden_words = True
+        has_forbidden_words = True
 
-    return have_forbidden_words
+    return has_forbidden_words
 
 
 def check_password_strength(password, forbidden_words):
     strength = 0
     if is_password_long(password):
         strength += 3
-    if have_both_upper_and_lower_cases(password):
+    if has_both_upper_and_lower_cases(password):
         strength += 2
-    if have_numerical_digits(password):
+    if has_numbers(password):
         strength += 3
-    if have_special_characters(password):
+    if has_special_characters(password):
         strength += 2
-    if have_forbidden_words(password, forbidden_words):
+    if has_forbidden_words(password, forbidden_words):
         strength /= 2
 
     return strength
 
 if __name__ == '__main__':
-    password = input('Введите пароль')
-    blacklist = input_from_file('Введите имя файла чёрного списка:')
-    personal_info = \
-        input_from_file('Введите имя файла с персональной информацией:')
-    company_name = \
-        input_from_file(
-            'Введите имя файла с полным и кратким названием компании:')
+    password = getpass.getpass('Введите пароль')
+
+    blacklist = get_added_data_from_file('Введите имя файла чёрного списка:')
+    personal_info = get_added_data_from_file(
+        'Введите имя файла с персональной информацией:')
+    company_name = get_added_data_from_file(
+        'Введите имя файла с полным и кратким названием компании:')
     forbidden_words = blacklist + personal_info + company_name
 
-    password_strength = \
-        check_password_strength(password, forbidden_words)
+    password_strength = check_password_strength(
+        password, forbidden_words)
 print('Оценка сложности пароля:   {}'.format(password_strength))
