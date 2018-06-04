186da7b97996679b50e8bf68d2b6853f109f40cd
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-04 22:11:18 +0300

    I nave made the script easier

--- password_strength.py
+++ password_strength.py
@@ -11,19 +11,15 @@ def is_password_in_blacklist(file_path='blacklist.txt'):
 
 
 def get_rating(password, password_rating):
-    min_password_len = 6
+    max_password_len = 6
+    if len(password) >= max_password_len:
+        password_rating += 2
     if any(char in punctuation for char in password):
-        password_rating += 1
-    if len(password) >= min_password_len:
         password_rating += 2
-    if len(password) == max_password_len:
-        password_rating += 2
-    if any(char.isupper() for char in password):
-        password_rating += 2
-    if any(char.islower() for char in password):
-        password_rating += 2
+    if any(char.isupper() and char.islower() for char in password):
+        password_rating += 3
     if any(char.isdigit() for char in password):
-        password_rating += 2
+        password_rating += 3
     if password in blacklist:
         password_rating /= 2
     return password_rating
@@ -40,7 +36,6 @@ def get_password():
 
 if __name__ == "__main__":
     password_rating = 0
-    max_password_len = 10
     password = get_password()
     blacklist = is_password_in_blacklist()
     password_rating = get_rating(
